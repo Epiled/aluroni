95eaa351d4c5a68e6ea18954cbd6be87e5ea9aa8
Felipe De Andrade <felipe.deam98@gmail.com>
2023-10-29 07:56:54 -0300

    Aula-3

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,9 +1,28 @@
+import Buscador from "./Buscador";
 import styles from "./Cardapio.module.scss";
+import { useState } from "react";
+
+import { ReactComponent as Logo } from "assets/logo.svg";
 
 export default function Cardapio() {
+    const [busca, setBusca] = useState("");
+
     return (
         <main>
-            <nav className="menu"></nav>
+            <nav className={styles.menu}>
+                <Logo className="Teste" width="152" height="64" title="Logo Aluroni" />
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa dos códigos e massas!
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>
+                    Cardápio
+                </h3>
+                <Buscador busca={busca} setBusca={setBusca} />
+            </section>
         </main>
     )
 }
