24258ce0a3457bfffd27e182a8d9c90a45a7815b
Felipe De Andrade <felipe.deam98@gmail.com>
2026-04-22 15:58:06 -0300

    refactor: complete migration from CRA to Vite

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,8 +1,8 @@
-import styles from './Sobre.module.scss';
-import stylesTema from 'styles/Tema.module.scss';
-import casa from 'assets/sobre/casa.webp';
-import massa1 from 'assets/sobre/massa1.webp';
-import massa2 from 'assets/sobre/massa2.webp';
+import styles from "./Sobre.module.scss";
+import stylesTema from "styles/Tema.module.scss";
+import casa from "@assets/sobre/casa.webp";
+import massa1 from "@assets/sobre/massa1.webp";
+import massa2 from "@assets/sobre/massa2.webp";
 
 const imagens = [massa1, massa2];
 
@@ -14,13 +14,20 @@ export default function Sobre() {
         <img src={casa} alt="Casa Aluroni" />
         <div className={styles.sobreNos__texto}>
           <p>
-            Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos pelos ingredientes tradicionais da culinária Italiana, frescos e de excelente qualidade para que sua experiência seja ainda mais intensa!
+            Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa
+            Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos
+            pelos ingredientes tradicionais da culinária Italiana, frescos e de
+            excelente qualidade para que sua experiência seja ainda mais
+            intensa!
           </p>
           <p>
-            Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto! 
+            Também possuímos uma cardápio de carnes com muitas opções de acordo
+            com o seu gosto!
           </p>
           <p>
-            Para acompanhar as massas italianas, Aluroni possui uma reserva de vinhos especiais, que harmonizam perfeitamente com o seu parto, seja carne ou massa!
+            Para acompanhar as massas italianas, Aluroni possui uma reserva de
+            vinhos especiais, que harmonizam perfeitamente com o seu parto, seja
+            carne ou massa!
           </p>
         </div>
       </div>
